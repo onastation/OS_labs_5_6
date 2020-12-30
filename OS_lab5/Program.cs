3fe1025f282065081518b1599e7f5d6affe2b809
onastation <imonastation@gmail.com>
2020-12-30 21:20:24 +0200

    working with memory optimization & profiling

---
+++ OS_lab5/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Diagnostics;
+namespace OS_lab5
+{
+    internal static class Program
+    {
+        private static void Main(string[] args)
+        {
+            var stopwatch = new Stopwatch();
+            stopwatch.Start();
+            int[,] AD = new int[2,2];
+            int res = 0;
+
+            for (int j = 500000000; j > 0; j--)
+            {
+                //AD[0, 0]++;
+                //AD[0, 0]++;
+                AD[0, 0] += 2;
+            }
+
+            //AD[1, 1] = AD[0, 0];
+
+            Console.WriteLine(AD[0, 0]);
+            stopwatch.Stop();
+            Console.WriteLine("Execution Time: " + stopwatch.Elapsed);
+        }
+
+    }
+}
---
+++ OS_lab6/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+namespace OS_lab6
+{
+    class Program
+    {
+        //void new_func1() { }
+
+        static void FirstFunction()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                //process 
+                Thread.Sleep(1000);
+            }
+
+            return;
+        }
+
+        static void SecondFunction()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                //process 
+                Thread.Sleep(1000);
+            }
+            return;
+        }
+
+        static void ThirdFunction()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                //process 
+                Thread.Sleep(1000);
+            }
+            return;
+        }
+        static void Main(string[] args)
+        {
+            var stopwatch = new Stopwatch();
+            stopwatch.Start();
+            Console.WriteLine("\n Inside main()\n");
+            string numoffunc;
+            numoffunc = Console.ReadLine();
+
+            //int i = 0;
+
+            //for (; i < 0xffffff; i++) ;
+
+
+            switch (numoffunc)
+            {
+                case "first":
+                    FirstFunction();
+                    break;
+                case "second":
+                    SecondFunction();
+                    break;
+
+                case "third":
+                    ThirdFunction();
+                    break;
+            }
+            
+            FirstFunction();
+            stopwatch.Stop();
+            Console.WriteLine("Execution Time: " + stopwatch.Elapsed);
+        }
+
+    }
+}
